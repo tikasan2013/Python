56caf29aa8bcd0e5113c5a89f25eb7b04b675171
tikasan2013 <tikasan2013@gmail.com>
2016-09-27 20:53:36 +0800

    Delete mssql_test.py

--- mssql_test.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-cn = pyodbc.connect('DRIVER={SQL Server};SERVER=10.100.1.183;DATABASE=pes;UID=sa;PWD=inn0d@t@')
-cur = cn.cursor();
-cur.execute("select top 10 userid, firstname from employees")
-row = cur.fetchone()
-print 'userid: ', row[0]
-print 'firstname: ', row.firstname
